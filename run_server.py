cbcdf242456096df4e236c8becde297451f5584c
K. J. B. Thilakarathna <168623163+KasunJanith@users.noreply.github.com>
2026-06-19 20:29:57 +0530

    changed

--- run_server.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-import os
-
-os.chdir(r'd:\My Github Projects\Cubematrix\Thinkcube-lottery-splitter-backend')
-
-if __name__ == "__main__":
-    print("Starting server on http://0.0.0.0:8000")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
